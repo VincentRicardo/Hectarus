9d32e8480ebf0eee55746072535a2e3ba368a94d
VincentRicardo <150823591+VincentRicardo@users.noreply.github.com>
2025-02-17 13:00:52 +0700

    Add files via upload

---
+++ src/hectarus_robot_controller/launch/all.launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+import launch_ros.actions
+
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package = 'hectarus_robot_controller', executable= 'process_node'),
+        launch_ros.actions.Node(
+            package = 'hectarus_robot_controller', executable = 'tripod_gait_node'),
+        launch_ros.actions.Node(
+            package = 'hectarus_robot_controller', executable = 'gait_node'),
+        launch_ros.actions.Node(
+            package = 'hectarus_robot_controller', executable = 'wave_gait_node'),
+        launch_ros.actions.Node(
+            package = 'hectarus_robot_controller', executable = 'tetrapod_gait_node'),
+        launch_ros.actions.Node(
+            package = 'hectarus_robot_controller', executable = 'turn_gait_node'),
+        launch_ros.actions.Node(
+            package = 'hectarus_robot_controller', executable = 'stop_gait_node'),
+        launch_ros.actions.Node(
+            package = 'hectarus_robot_controller', executable = 'ultrasonik_node'),
+        launch_ros.actions.Node(
+            package = 'hectarus_robot_controller', executable = 'gyro_node')
+        ])
